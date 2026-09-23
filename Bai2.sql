1d63371fb521acbd92d9b942aae4a6efe379752c
dinhmanhctnk33 <dinhmamato@gmail.com>
2026-09-23 14:40:56 +0700

    Cap nhat file Bai2.sql

--- Bai2.sql
+++ Bai2.sql
@@ -7,3 +7,15 @@ create table Student(
     age int,
     country varchar(50)
 );
+
+create table Class(
+    id int,
+    name varchar(200)
+);
+
+create table Teacher(
+    id int,
+    name varchar(200),
+    age int,
+    country varchar(50)
+);
